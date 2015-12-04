425e803fc96e3431edfc003ec4a1bda6f5392d24
AkshataMohanty <akshatamohanty@gmail.com>
2015-12-05 05:43:22 +0800

    cuisine feature

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-var app = angular.module('lunchedIn', ['ngMaterial']);
+var app = angular
+      .module('lunchedIn', ['ngMaterial'])
 
 app.controller('UserDisplay', [
 	'$scope',
@@ -34,4 +35,77 @@ app.controller('UserDisplay', [
 
 	}
 
+]);
+
+app.controller('preferenceFormCtrl', [
+			'$scope',
+			function( $scope ){
+				
+				// cuisine preference
+
+			    $scope.selectedItem = null;
+				$scope.searchText = null;
+				$scope.querySearch = querySearch;
+				$scope.cuisines = loadCuisines();
+				$scope.selectedCuisines = [];
+				$scope.transformChip = transformChip;
+		    
+			    /**
+			     * Return the proper object when the append is called.
+			     */
+			    function transformChip(chip) {
+			      // If it is an object, it's already a known chip
+			      if (angular.isObject(chip)) {
+			        return chip;
+			      }
+			    }
+			    /**
+			     * Search for cuisines.
+			     */
+			    function querySearch (query) {
+			      var results = query ? $scope.cuisines.filter(createFilterFor(query)) : [];
+			      return results;
+			    }
+			    /**
+			     * Create filter function for a query string
+			     */
+			    function createFilterFor(query) {
+			      var lowercaseQuery = angular.lowercase(query);
+			      return function filterFn(cuisine) {
+					        return (cuisine._lowername.indexOf(lowercaseQuery) === 0)
+					     };
+			    }
+
+			    function loadCuisines() {
+			      var cuisines = [
+			        {
+			          'name': 'Indian',
+			          'resCount': 12
+			        },
+			        {
+			          'name': 'Chinese',
+			          'resCount': 15
+			        },
+			        {
+			          'name': 'Japanese',
+			          'resCount': 2
+			        },
+			        {
+			          'name': 'Thai',
+			          'resCount': 4
+			        },
+			        {
+			          'name': 'Italian',
+			          'resCount': 7
+			        }
+			      ];
+
+			      return cuisines.map(function (c) {
+			        c._lowername = c.name.toLowerCase();
+			        return c;
+			      });
+
+			    }
+
+  			}
 ]);
