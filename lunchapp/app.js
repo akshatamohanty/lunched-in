b1ba6578ee8ee4ce3cc64e02de000c90494beb74
AkshataMohanty <akshatamohanty@gmail.com>
2016-01-17 02:59:00 +0800

    MongoSchemas, REST API and Angular - working

--- lunchapp/app.js
+++ lunchapp/app.js
@@ -17,7 +17,7 @@ var methodOverride = require('method-override');
 
 
 // configuration ==============================
-mongoose.connect('mongodb://127.0.0.1:27017/data');
+mongoose.connect('mongodb://127.0.0.1:27017/test');
 
 
 app.use(express.static(__dirname + '/public'));
@@ -29,14 +29,145 @@ app.use(methodOverride());
 
 // define model ========================
 var User = mongoose.model('User', {
-  name: String,
+  uid: Number,
+  name: String , 
+  title: String,
+  phone: String,
   tagline: String,
+  picture: String,
+  cuisine: [String]
 });
+
+var Lunch = mongoose.model('Lunch', {
+  date: String , 
+  restaurant: String,
+  participants: [Number]
+});
+
+// populating the model with the userDatabase
+dummyUsers = [{
+    uid: 1, 
+    name: "James Potter",
+    title: "Senior Architect",
+    phone: "902xx",
+    tagline: "Prongs",
+    picture: "http://u.lorenzoferrara.net/marlenesco/material-card/thumb-christopher-walken.jpg",
+    cuisine: ["chinese"], 
+    blocked: []
+  },
+  {
+    uid: 2, 
+    name: "Sirius Black",
+    title: "Junior Architect",
+    phone: "902xx",
+    tagline: "Padfoot",
+    picture: "http://u.lorenzoferrara.net/marlenesco/material-card/thumb-christopher-walken.jpg",
+    cuisine: ["italian"],
+    blocked: []
+  },
+  {
+    uid: 3, 
+    name: "Peter Pettigrew",
+    title: "Finance",
+    phone: "902xx",
+    tagline: "Wormtail",
+    picture: "http://u.lorenzoferrara.net/marlenesco/material-card/thumb-christopher-walken.jpg",
+    cuisine: ["mexican"],
+    blocked: []
+  },
+  {
+    uid:4, 
+    name: "Albus Dumbledore",
+    title: "Director",
+    phone: "902xx",
+    tagline: "Phoenix",
+    picture: "http://u.lorenzoferrara.net/marlenesco/material-card/thumb-christopher-walken.jpg",
+    cuisine: ["indian"],
+    blocked: []
+  },
+  {
+    uid: 5, 
+    name: "Remus Lupin",
+    title: "HR",
+    phone: "902xx",
+    tagline: "Moony",
+    picture: "http://u.lorenzoferrara.net/marlenesco/material-card/thumb-christopher-walken.jpg",
+    cuisine: ["thai"],
+    blocked: []
+  }
+]
+
+dummyLunch = [
+  {
+    date: "date1" , 
+    restaurant: "res1",
+    participants: [1, 2, 5, 3]
+  },
+  {
+    date: "date2" , 
+    restaurant: "res2",
+    participants: [5, 2, 4, 1]
+  }
+]
+
+for(var i=0; i<5; i++){
+
+  // populating 5 dummy users
+  User.create(
+        {   
+            'uid': dummyUsers[i].uid,
+            'name': dummyUsers[i].name,
+            'title': dummyUsers[i].title,
+            'phone': dummyUsers[i].phone,
+            'tagline': dummyUsers[i].tagline,
+            'picture': dummyUsers[i].picture,
+            'cuisine': dummyUsers[i].cuisine
+        }
+        , function(err, user){
+
+            if(err)
+              console.log(err);
+
+            User.find(function(err, users){
+                
+                if(err)
+                    console.log(err)
+
+                console.log(users);
+            });
+        });
+
+  // populating 2 dummy lunches
+  if( i < 2){
+    Lunch.create(
+          {   
+              'date': dummyLunch[i].date, 
+              'restaurant': dummyLunch[i].restaurant,
+              'participants': dummyLunch[i].participants
+          }
+          , function(err, user){
+
+              if(err)
+                console.log(err);
+
+              Lunch.find(function(err, lunches){
+                  
+                  if(err)
+                      console.log(err)
+
+                  console.log(lunches);
+              });
+          });
+  }
+}
+
+console.log(dummyUsers.length + ' dummy users populated');
+console.log(dummyLunch.length + ' dummy lunches populated');
 
 // routes ================
 
    //api -------------
-   app.get('/api/users', function(req, res){
+   app.get('/api/users/:user_id', function(req, res){
 
       //use mongoose to get all users in the database
       User.find(function(err, users){
@@ -44,17 +175,39 @@ var User = mongoose.model('User', {
             if(err)
               res.send(err);
 
+            console.log("hey there", users);
             res.json(users);
 
       });
-   })
+   });
 
-   // creating a new user
+  app.get('/api/user_pref/:user_id', function(req, res){
+
+      //use mongoose to get the particular user
+      User.find({ 
+        uid : req.params.user_id
+      },function(err, user_details){
+
+            if(err)
+              res.send(err);
+
+            res.json(user_details);
+      });
+
+   });
+
+/*   // creating a new user
    app.post('/api/users', function(req, res){
 
-        User.create({
-              name: req.body.text, 
-        }, function(err, user){
+        User.create(
+        {   'name': req.name,
+            'title': req.title,
+            'phone': req.phone,
+            'tagline': req.tagline,
+            'picture': req.picture,
+            'cuisine': req.cuisine
+        }
+        , function(err, user){
 
             if(err)
               res.send(err);
@@ -67,12 +220,12 @@ var User = mongoose.model('User', {
                 res.json(users);
             });
         });
- });
+ });*/
 
    // deleting a user
    app.delete('/api/users/:user_id', function(req, res){
       User.remove({
-        _id : req.params.user_id
+        _id : req.params
       }, function( err, user ){
          if(err)
            res.send(err);
@@ -86,8 +239,26 @@ var User = mongoose.model('User', {
       });
    });
 
-   app.get('*', function(req, res){
-        res.sendfile('./public/index.html');
+   // getting today's lunch match for a user
+  app.get('/api/matches/:user_id', function(req, res){
+      
+      //use mongoose to get all lunches for this user in the database
+      Lunch.find( { 
+        participants : req.params.user_id
+      }, function(err, lunches){
+
+            if(err)
+              res.send(err);
+
+            res.json(lunches);
+
+      });
+
+   });
+
+
+  app.get('*', function(req, res){
+        res.sendFile('./public/index.html');
    });
 
 // listen (start app with node app.js)
--- lunchapp/public/angularApp.js
+++ lunchapp/public/angularApp.js
@@ -1,119 +1,76 @@
-/*var userDB = [{
-		'name': "James Potter",
-		'title': "Senior Architect",
-		'phone': "902xx",
-		'tagline': "Prongs",
-		'enabled': true,
-		'picture': 'http://u.lorenzoferrara.net/marlenesco/material-card/thumb-christopher-walken.jpg',
-		'cuisine': []
-	},
-	{
-		'name': "Sirius Black",
-		'title': "Junior Architect",
-		'phone': "902xx",
-		'tagline': "Padfoot",
-		'enabled': false,
-		'picture': 'http://u.lorenzoferrara.net/marlenesco/material-card/thumb-christopher-walken.jpg',
-		'cuisine': []
-	},
-	{
-		'name': "Peter Pettigrew",
-		'title': "Finance",
-		'phone': "902xx",
-		'tagline': "Wormtail",
-		'enabled': false,
-		'picture': 'http://u.lorenzoferrara.net/marlenesco/material-card/thumb-christopher-walken.jpg',
-		'cuisine': []
-	},
-	{
-		'name': "Albus Dumbledore",
-		'title': "Director",
-		'phone': "902xx",
-		'tagline': "Phoenix",
-		'enabled': true,
-		'picture': 'http://u.lorenzoferrara.net/marlenesco/material-card/thumb-christopher-walken.jpg',
-		'cuisine': []
-	},
-	{
-		'name': "Remus Lupin",
-		'title': "HR",
-		'phone': "902xx",
-		'tagline': "Moony",
-		'enabled': true,
-		'picture': 'http://u.lorenzoferrara.net/marlenesco/material-card/thumb-christopher-walken.jpg',
-		'cuisine': []
-	}
-]*/
+
 
 var app = angular
-      .module('lunchedIn', ['ngMaterial', 'ngRoute', 'ngMdIcons'])
+      .module("lunchedIn", ["ngMaterial", "ngRoute", "ngMdIcons"])
 
-app.config(['$routeProvider', function($routeProvider) {
+app.config(["$routeProvider", function($routeProvider) {
    $routeProvider.
    
-   when('/mates', {
-      templateUrl: 'templates/mates.html', controller: 'UserDisplay'
+   when("/mates/:param", {
+      templateUrl: "templates/mates.html", 
+      controller: "UserDisplay"
    }).
    
-   when('/preferences', {
-      templateUrl: 'templates/preferences.html', controller: 'preferenceFormCtrl'
+   when("/preferences/:param", {
+      templateUrl: "templates/preferences.html", 
+      controller: "preferenceFormCtrl"
    }).
 
-   when('/matches', {
-      templateUrl: 'templates/matches.html', controller: 'preferenceFormCtrl'
+   when("/matches/:param", {
+      templateUrl: "templates/matches.html", 
+      controller: "MatchesDisplayCtrl"
    }).
    
    otherwise({
-      redirectTo: '/matches'
+      redirectTo: "/matches/1"
    });
 	
 }]);
 
 
-app.controller('UserDisplay', [ 
-	'$scope', '$http',
-	function( $scope, $http ){
+app.controller("UserDisplay", [ 
+	"$scope", "$http", "$routeParams",
+	function( $scope, $http, $routeParams ){
 
 		var userDB = [];
 
-		$http.get('/api/users')
+		$http.get("/api/users/" + $routeParams.param)
 			.success( function(data){
 				userDB = data;
 				console.log(data);
-			})
-			.error(function(data){
-				console.log('Error:' + data);
-			});
 
-		$scope.load = function(){
-			$('.material-card > .mc-btn-action').click(function () { 
-			                      var card = $(this).parent('.material-card');
-			                      var icon = $(this).children('i');
-			                      icon.addClass('fa-spin-fast');
+				//jquery
+				$(".material-card > .mc-btn-action").click(function () { 
+			                      var card = $(this).parent(".material-card");
+			                      var icon = $(this).children("i");
+			                      icon.addClass("fa-spin-fast");
 
-			                      if (card.hasClass('mc-active')) {
-			                          card.removeClass('mc-active');
+			                      if (card.hasClass("mc-active")) {
+			                          card.removeClass("mc-active");
 
 			                          window.setTimeout(function() {
 			                              icon
-			                                  .removeClass('fa-arrow-left')
-			                                  .removeClass('fa-spin-fast')
-			                                  .addClass('fa-bars');
+			                                  .removeClass("fa-arrow-left")
+			                                  .removeClass("fa-spin-fast")
+			                                  .addClass("fa-bars");
 
 			                          }, 800);
 			                      } else {
-			                          card.addClass('mc-active');
+			                          card.addClass("mc-active");
 
 			                          window.setTimeout(function() {
 			                              icon
-			                                  .removeClass('fa-bars')
-			                                  .removeClass('fa-spin-fast')
-			                                  .addClass('fa-arrow-left');
+			                                  .removeClass("fa-bars")
+			                                  .removeClass("fa-spin-fast")
+			                                  .addClass("fa-arrow-left");
 
 			                          }, 800);
 			                      }
 			                  });
-		}
+			})
+			.error(function(data){
+				console.log("Error:" + data);
+			});
 		
 		var chars = "ABCDEFGHIJKLMNOPQURSTUVWXYZ";
 		var tabs = [];
@@ -142,7 +99,7 @@ app.controller('UserDisplay', [
 	     * Return the proper object when the append is called.
 	     */
 	    function transformChip(chip) {
-	      // If it is an object, it's already a known chip
+	      // If it is an object, it"s already a known chip
 	      if (angular.isObject(chip)) {
 	        return chip;
 	      }
@@ -167,11 +124,27 @@ app.controller('UserDisplay', [
 	    //$scope.load();
 	}
 
-])
+]);
 
-app.controller('preferenceFormCtrl', [
-			'$scope',
-			function( $scope ){
+app.controller("preferenceFormCtrl", [
+			"$scope", "$http", "$routeParams",
+			function( $scope, $http, $routeParams ){
+
+				$scope.userDetails = {}; 
+
+				// user preference options
+				$http.get("/api/user_pref/" + $routeParams.param)
+						.success( function(data){
+
+							$scope.userDetails = data[0];
+							//console.log("preference", data);
+
+						})
+						.error(function(data){
+							console.log("Error:" + data);
+						});
+		
+
 				$scope.message = "This page will be used to display add preferences";
 				// cuisine preference
 
@@ -186,7 +159,7 @@ app.controller('preferenceFormCtrl', [
 			     * Return the proper object when the append is called.
 			     */
 			    function transformChip(chip) {
-			      // If it is an object, it's already a known chip
+			      // If it is an object, it"s already a known chip
 			      if (angular.isObject(chip)) {
 			        return chip;
 			      }
@@ -212,88 +185,88 @@ app.controller('preferenceFormCtrl', [
 			   	function loadCuisines() {
 						      var cuisines = [
 						        {
-						          'name': 'American',
-						          'icon': 'pictureaddress'
+						          "name": "American",
+						          "icon": "pictureaddress"
 						        },
 						        {
-						          'name': 'Chinese',
-						          'icon': 'pictureaddress'
+						          "name": "Chinese",
+						          "icon": "pictureaddress"
 						        },
 						        {
-						          'name': 'Continental',
-						          'icon': 'pictureaddress'
+						          "name": "Continental",
+						          "icon": "pictureaddress"
 						        },
 						        {
-						          'name': 'Cuban',
-						          'icon': 'pictureaddress'
+						          "name": "Cuban",
+						          "icon": "pictureaddress"
 						        },
 						        {
-						          'name': 'French',
-						          'icon': 'pictureaddress'
+						          "name": "French",
+						          "icon": "pictureaddress"
 						        },
 						        {
-						          'name': 'Greek',
-						          'icon': 'pictureaddress'
+						          "name": "Greek",
+						          "icon": "pictureaddress"
 						        },
 						        {
-						          'name': 'Indian',
-						          'icon': 'pictureaddress'
+						          "name": "Indian",
+						          "icon": "pictureaddress"
 						        },
 						        {
-						          'name': 'Indonesian',
-						          'icon': 'pictureaddress'
+						          "name": "Indonesian",
+						          "icon": "pictureaddress"
 						        },
 						        {
-						          'name': 'Italian',
-						          'icon': 'pictureaddress'
+						          "name": "Italian",
+						          "icon": "pictureaddress"
 						        },
 						        {
-						          'name': 'Japanese',
-						          'icon': 'pictureaddress'
+						          "name": "Japanese",
+						          "icon": "pictureaddress"
 						        },
 						        {
-						          'name': 'Korean',
-						          'icon': 'pictureaddress'
+						          "name": "Korean",
+						          "icon": "pictureaddress"
 						        },
 						        {
-						          'name': 'Lebanese',
-						          'icon': 'pictureaddress'
+						          "name": "Lebanese",
+						          "icon": "pictureaddress"
 						        },
 						        {
-						          'name': 'Malaysian',
-						          'icon': 'pictureaddress'
+						          "name": "Malaysian",
+						          "icon": "pictureaddress"
 						        },
 						        {
-						          'name': 'Mexican',
-						          'icon': 'pictureaddress'
+						          "name": "Mexican",
+						          "icon": "pictureaddress"
 						        },
 						        {
-						          'name': 'Pakistani',
-						          'icon': 'pictureaddress'
+						          "name": "Pakistani",
+						          "icon": "pictureaddress"
 						        },
 						        {
-						          'name': 'Russian',
-						          'icon': 'pictureaddress'
+						          "name": "Russian",
+						          "icon": "pictureaddress"
 						        },
 						        {
-						          'name': 'Singaporean',
-						          'icon': 'pictureaddress'
+						          "name": "Singaporean",
+						          "icon": "pictureaddress"
 						        },
 						        {
-						          'name': 'Spanish',
-						          'icon': 'pictureaddress'
+						          "name": "Spanish",
+						          "icon": "pictureaddress"
 						        },
 						        {
-						          'name': 'Thai',
-						          'icon': 'pictureaddress'
+						          "name": "Thai",
+						          "icon": "pictureaddress"
 						        },
 						        {
-						          'name': 'Tibetan',
-						          'icon': 'pictureaddress'
+						          "name": "Tibetan",
+						          "icon": "pictureaddress"
 						        },
 						        {
-						          'name': 'Vietnamese',
-						          'icon': 'pictureaddress'
+						          "name": "Vietnamese",
+						          "icon": "pictureaddress"
 						        }
 
 						      ];
@@ -308,3 +281,39 @@ app.controller('preferenceFormCtrl', [
   			}
 ]);
 
+
+app.controller("MatchesDisplayCtrl", [
+			"$scope", "$http", "$routeParams",
+			function( $scope, $http, $routeParams ){
+				$scope.message = "This page will be used to display add preferences";
+
+				$scope.lunches = [];
+
+				$http.get("/api/matches/" + $routeParams.param )
+						.success( function(data){
+							$scope.lunches = data; 
+							console.log(data);
+						})
+						.error( function( data ){
+							console.log("Error: ", data);
+						});
+				
+				// checking for lunch today
+				for(var i=0; i < $scope.lunches.length; i++ ){
+					var lunch = $scope.lunches[i];
+					if(lunch.date == "date1"){
+						$scope.today = lunch;
+						$scope.today.message = "Lunch today!";
+						$scope.lunches.splice(i, 1);
+					}
+					else
+						$scope.today = {
+							"message" : "No Lunch Today",
+							"date" : "",
+							"participants": "",
+							"restaurant": ""
+						}
+				}
+			}
+
+]);
