5eacf0f0ece3df61afc0eee9f6352908e6888364
AkshataMohanty <akshatamohanty@gmail.com>
2016-04-05 23:22:50 +0800

    lunchmates dashboard

--- public/angularApp.js
+++ public/angularApp.js
@@ -36,8 +36,8 @@ app.config(["$routeProvider", function($routeProvider) {
    }).
 
    otherwise({
-      templateUrl: "templates/lunches.html", 
-      controller: "lunchesCtrl"
+      templateUrl: "templates/lunchMates.html", 
+      controller: "lunchMatesCtrl"
    });
 	
 }]);
@@ -284,23 +284,14 @@ app.controller("settingsCtrl", [
 
 
 /**************** User Display Controller *****************/
-app.controller("lunchMatesCtrl", [ 
-   "$scope", "$http", 
-   function( $scope, $http ){
+app.controller("lunchMatesCtrl", [
+  "$scope", "$http", 
+  function( $scope, $http ){
 
       $scope.users = [];
-      $scope.userDetails = {};
 
-      $scope.newUser = {  'name': 'John Doe', 
-                     'email': 'johndoe@aedas.sg', 
-                     'password': 'pass',
-                     'title': 'Senior Designer',
-                     'cuisine': ['Chinese', 'Indian'],
-                     'available' : ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'] 
-                   };
-
-      // get all users data
-      function refreshUserList(){
+      // gets all user details - depending on if you are admin or normal user
+      var refreshUserList = function(){
          $http.get("/api/users")
             .success( function(data){
 
@@ -313,128 +304,37 @@ app.controller("lunchMatesCtrl", [
             });
       }
 
-      // get current user data
-      $http.get("/api/user_pref")
-            .success( function(data){
+      refreshUserList();
 
-               if(data.statusCode == 302){
-                  $window.location.href = '/login'
-               }
+}]);
 
-               $scope.userDetails = data;
-               //console.log("preference", data);
 
-            })
-            .error(function(data){
-               console.log("Error:" + data);
-            });
+app.controller("lunchMatesCtrlUser", [
+  "$scope", "$http", 
+  function( $scope, $http ){
+      
+      //add or block functionality
 
-      var chars = "ABCDEFGHIJKLMNOPQURSTUVWXYZ";
-      var tabs = [];
-      for(var c=0; c<26; c++){
-         var t = {};
-         t.title = chars[c];
-         tabs.push(t);
-      }
-      $scope.tabs = tabs;
-      $scope.selectedIndex = 0;
+  }]);
 
-      $scope.message = "This page will be used to display users";
-      $scope.users = $scope.users.map(function (c) {
-                     c._lowername = c.name.toLowerCase();
-                     return c;
-                     });
 
-      $scope.selectedItem = null;
-      $scope.searchText = null;
-      $scope.querySearch = querySearch;
-      $scope.selectedUsers = [];
-      $scope.transformChip = transformChip;
+app.controller("lunchMatesCtrlAdmin", [ 
+   "$scope", "$http", 
+   function( $scope, $http ){
 
-      $scope.checkInitial = checkInitial; 
-
-      $scope.containsObject = containsObject; 
-      $scope.addToList = addToList; 
-
-      $scope.saveMates = saveMates;
-
+      // add user / delete user / edit user functionality
       $scope.addNewUser = addNewUser;
       $scope.deleteUser = deleteUser;
+      $scope.newUser = {
+        'name': 'New User',
+        'email': 'newuser@aedas.in',
+        'password': 'password',
+        'title': 'Not Provided'
+      } 
 
-      // http post to modify mates
-      function saveMates(){
-         $http.post('/api/edit_mates', $scope.userDetails)
-                      .success(function(data){
-                        console.log($scope.userDetails._id);
-                        console.log("user updated", data);
-                      })
-                      .error(function(data){
-                        console.log('Error:', data);
-                      });
-      }
+      $scope.selectedUser = {'name': 'No User Selected'};
 
-      function containsObject(obj, list) {
-          var i;
-          for (i = 0; i < list.length; i++) {
-              if (list[i]._id == obj._id) {
-               //console.log(obj.name, "is contained in", list)
-                  return true;
-              }
-          }
-
-          //console.log(obj.name, "is not contained in", list);
-          return false;
-      }
-
-      function addToList( user, list ){
-
-         if ( containsObject( user, $scope.userDetails[list] ) )
-            console.log("Already ", list)
-         else 
-            $scope.userDetails[list].push(user);
-
-      }
-
-      function checkInitial( intial, string ){
-         // check if the user is the current - dont display self to self!
-         if( $scope.userDetails.name == string ){
-            //console.log("Self detected!")
-            return false; 
-         }
-
-         var result = angular.lowercase(string[0]) == angular.lowercase(intial);
-         return result;
-      }
-    
-       /**
-        * Return the proper object when the append is called.
-        */
-       function transformChip(chip) {
-         // If it is an object, it"s already a known chip
-         if (angular.isObject(chip)) {
-           return chip;
-         }
-       }
-       /**
-        * Search for cuisines.
-        */
-       function querySearch (query) {
-         if($scope.users.length == 0)
-            return;
-         var results = query ? $scope.users.filter(createFilterFor(query)) : [];
-         return results;
-       }
-       /**
-        * Create filter function for a query string
-        */
-       function createFilterFor(query) {
-         var lowercaseQuery = angular.lowercase(query); 
-         return function filterFn(user) {
-                  return (user._lowername.indexOf(lowercaseQuery) === 0)
-              };
-       }
-
-       function addNewUser(){
+      var addNewUser = function(){
          $http.post('/api/addUser', $scope.newUser)
              .success( function(data){
                console.log("User Added. ", data);
@@ -445,9 +345,16 @@ app.controller("lunchMatesCtrl", [
              })
 
          refreshUserList();
-       }
+      }
 
-       function deleteUser( email ){
+      var editUser = function( email ){
+
+         //http post to edit
+
+         refreshUserList();
+      }
+
+      var deleteUser = function( email ){
 
          $http.post('/api/removeUser', {'email' : email } )
              .success( function(data){
@@ -459,7 +366,7 @@ app.controller("lunchMatesCtrl", [
              })
 
          refreshUserList();
-       }
+      }
    }
 
 ]);
